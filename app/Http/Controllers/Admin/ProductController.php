33bd5c2dfb9e2492d4d45d05257e0ec0143768b9
Prasaja <prasaja55@gmail.com>
2025-10-13 00:11:34 +0700

    update and fix the code

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -32,16 +32,6 @@ class ProductController extends Controller
         return view('backend.products.index', compact('products'));
     }
 
-    public function home()
-    {
-        $products = Product::where('status_active', 1)
-            ->orderBy('id', 'desc')
-            ->take(4)
-            ->get();
-
-        return view('frontend.home', compact('products'));
-    }
-
     public function create()
     {
         $fruits = FruitType::where('is_active', 1)->get();
