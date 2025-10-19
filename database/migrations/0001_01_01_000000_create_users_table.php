7536385a3f5c35353555e75812f919a3d1d53c70
rareaqil <rareaqil123@gmail.com>
2025-10-19 10:52:41 +0700

    add new role management and setting web.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
@@ -27,13 +26,12 @@ return new class extends Migration
             // $table->unsignedBigInteger('village_id')->nullable();
             // $table->string('postcode', 10)->nullable();
 
-            $table->enum('role', ['super_admin', 'admin', 'user'])->default('user');
+            $table->enum('role', ['super_admin', 'admin_toko', 'admin_kebun', 'user'])->default('user');
             $table->timestamps();
         });
 
-
         // Tabel Alamat user
-      Schema::create('user_addresses', function (Blueprint $table) {
+        Schema::create('user_addresses', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
 
--- routes/web.php
+++ routes/web.php
@@ -83,15 +83,6 @@ Route::middleware('auth')->group(function () {
 | Role-based Dashboards
 |--------------------------------------------------------------------------
 */
-// Super Admin
-Route::middleware(['auth', 'role:super_admin'])->group(function () {
-    Route::get('/superadmin/dashboard', fn() => 'Super Admin Dashboard')->name('superadmin.dashboard');
-});
-
-// Admin
-Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/backend/dashboard', fn() => view('dashboard'))->name('dashboard');
-});
 
 // User
 Route::middleware(['auth', 'role:user'])->group(function () {
@@ -104,9 +95,11 @@ Route::middleware(['auth', 'role:user'])->group(function () {
 | Backend (Super Admin & Admin)
 |--------------------------------------------------------------------------
 */
-Route::middleware(['auth', 'role:super_admin,admin'])
+Route::middleware(['auth', 'role:super_admin,admin_toko,admin_kebun'])
     ->prefix('backend')
     ->group(function () {
+        Route::get('/dashboard', fn() => view('dashboard'))->name('dashboard');
+
         // Users, Posts, Products (resource routes)
         Route::resource('users', UserController::class);
         Route::resource('posts', PostController::class);
