01a19929db232422a6f370fcde80d0856a0d2f3a
rareaqil <rareaqil123@gmail.com>
2025-12-21 23:03:04 +0700

    fix selected fruit type on product

--- resources/views/backend/products/form.blade.php
+++ resources/views/backend/products/form.blade.php
@@ -7,12 +7,8 @@
 
     <div class="mt-6 space-y-6 rounded-lg bg-white p-4 shadow sm:rounded-xl sm:p-8">
         <div class="mx-auto max-w-3xl space-y-6 rounded-xl bg-white p-6 shadow">
-            <form
-                action="{{ isset($product) ? route('products.update', $product) : route('products.store') }}"
-                method="POST"
-                enctype="multipart/form-data"
-                class="space-y-6"
-            >
+            <form action="{{ isset($product) ? route('products.update', $product) : route('products.store') }}"
+                method="POST" enctype="multipart/form-data" class="space-y-6">
                 @csrf
                 @if (isset($product))
                     @method('PUT')
@@ -21,29 +17,17 @@
                 {{-- Nama Produk --}}
                 <div>
                     <x-input-label required for="name" :value="'Nama Produk'" />
-                    <x-text-input
-                        id="name"
-                        name="name"
-                        type="text"
-                        class="mt-1 block w-full"
-                        value="{{ old('name', $product->name ?? '') }}"
-
-                    />
+                    <x-text-input id="name" name="name" type="text" class="mt-1 block w-full"
+                        value="{{ old('name', $product->name ?? '') }}" />
                     <x-input-error class="mt-2" :messages="$errors->get('name')" />
                 </div>
 
                 {{-- Deskripsi --}}
                 <div>
                     <x-input-label for="description" :value="'Deskripsi Produk'" />
-                    <textarea
-                        id="description"
-                        name="description"
-                        class="mt-1 block w-full rounded border-gray-300"
-                        rows="4"
-                        placeholder="Masukkan deskripsi singkat..."
-                    >
-{{ old('description', $product->description ?? '') }}</textarea
-                    >
+                    <textarea id="description" name="description" class="mt-1 block w-full rounded border-gray-300"
+                        rows="4" placeholder="Masukkan deskripsi singkat...">
+{{ old('description', $product->description ?? '') }}</textarea>
                     <x-input-error class="mt-2" :messages="$errors->get('description')" />
                 </div>
 
@@ -51,33 +35,20 @@
                     {{-- Harga --}}
                     <div>
                         <x-input-label required for="price" :value="'Harga (Rp)'" />
-                        <x-text-input
-                            id="price"
-                            name="price"
-                            type="number"
-                            step="1"
-                            min="0"
-                            class="mt-1 block w-full"
-                            value="{{ old('price', $product->price ?? '') }}"
-
-                        />
+                        <x-text-input id="price" name="price" type="number" step="1" min="0"
+                            class="mt-1 block w-full" value="{{ old('price', $product->price ?? '') }}" />
                         <x-input-error class="mt-2" :messages="$errors->get('price')" />
                     </div>
 
                     {{-- Jenis Buah --}}
                     <div>
                         <x-input-label for="fruit_type_id" :value="'Jenis Buah'" />
-                        <select
-                            id="fruit_type_id"
-                            name="fruit_type_id"
-                            class="select2 mt-1 block w-full rounded border-gray-300"
-                        >
+                        <select id="fruit_type_id" name="fruit_type_id"
+                            class="select2 mt-1 block w-full rounded border-gray-300">
                             <option value="">-- Tidak ada --</option>
                             @foreach ($fruits as $fruit)
-                                <option
-                                    value="{{ $fruit->id }}"
-                                    {{ old('fruit_type_id', $post->fruit_type_id ?? '') == $fruit->id ? 'selected' : '' }}
-                                >
+                                <option value="{{ $fruit->id }}"
+                                    {{ old('fruit_type_id', $product->fruit_type_id ?? '') == $fruit->id ? 'selected' : '' }}>
                                     {{ $fruit->name }}
                                 </option>
                             @endforeach
@@ -88,14 +59,8 @@
                     {{-- Stok --}}
                     <div class="hidden">
                         <x-input-label for="stock" :value="'Stok'" />
-                        <x-text-input
-                            id="stock"
-                            name="stock"
-                            type="number"
-                            class="mt-1 block w-full"
-                            value="{{ old('stock', $product->stock ?? 0) }}"
-                            required
-                        />
+                        <x-text-input id="stock" name="stock" type="number" class="mt-1 block w-full"
+                            value="{{ old('stock', $product->stock ?? 0) }}" required />
                         <x-input-error class="mt-2" :messages="$errors->get('stock')" />
                     </div>
                 </div>
@@ -120,13 +85,8 @@
                     {{-- SKU --}}
                     <div>
                         <x-input-label for="sku" :value="'SKU / Kode Unik'" />
-                        <x-text-input
-                            id="sku"
-                            name="sku"
-                            type="text"
-                            class="mt-1 block w-full"
-                            value="{{ old('sku', $product->sku ?? '') }}"
-                        />
+                        <x-text-input id="sku" name="sku" type="text" class="mt-1 block w-full"
+                            value="{{ old('sku', $product->sku ?? '') }}" />
                         <x-input-error class="mt-2" :messages="$errors->get('sku')" />
                     </div>
                 </div>
@@ -135,16 +95,12 @@
                 <div>
                     <x-input-label for="status_active" :value="'Status Produk'" />
                     <select id="status_active" name="status_active" class="mt-1 block w-full rounded border-gray-300">
-                        <option
-                            value="1"
-                            {{ old('status_active', $product->status_active ?? true) ? 'selected' : '' }}
-                        >
+                        <option value="1"
+                            {{ old('status_active', $product->status_active ?? true) ? 'selected' : '' }}>
                             Aktif
                         </option>
-                        <option
-                            value="0"
-                            {{ old('status_active', $product->status_active ?? true) == false ? 'selected' : '' }}
-                        >
+                        <option value="0"
+                            {{ old('status_active', $product->status_active ?? true) == false ? 'selected' : '' }}>
                             Nonaktif
                         </option>
                     </select>
@@ -168,31 +124,40 @@
             .select2-container .select2-selection--single {
                 height: 95%;
             }
+
             .select2-container--classic .select2-selection--single {
                 background-color: #fff;
-                border: 1px solid rgb(209 213 219); /* gray-300 */
-                border-radius: 0.25rem; /* rounded */
-                padding: 0.5rem 2.5rem 0.5rem 0.75rem; /* vertical, right for arrow */
+                border: 1px solid rgb(209 213 219);
+                /* gray-300 */
+                border-radius: 0.25rem;
+                /* rounded */
+                padding: 0.5rem 2.5rem 0.5rem 0.75rem;
+                /* vertical, right for arrow */
                 font-size: 1rem;
                 line-height: 1.5rem;
-                color: #111827; /* gray-900 */
+                color: #111827;
+                /* gray-900 */
                 background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 20 20'%3e%3cpath stroke='%236b7280' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M6 8l4 4 4-4'/%3e%3c/svg%3e");
                 background-repeat: no-repeat;
                 background-position: right 0.5rem center;
                 background-size: 1.5em 1.5em;
             }
+
             .select2-container--classic .select2-selection__rendered {
-                padding-left: 0; /* sudah di-handle container */
+                padding-left: 0;
+                /* sudah di-handle container */
             }
+
             .select2-container--classic .select2-selection__arrow {
-                display: none; /* kita pakai svg custom di atas */
+                display: none;
+                /* kita pakai svg custom di atas */
             }
         </style>
     @endpush
 
     @push('scripts')
         <script type="module">
-            document.addEventListener('DOMContentLoaded', function () {
+            document.addEventListener('DOMContentLoaded', function() {
                 $('#fruit_type_id').select2({
                     theme: 'classic',
                     placeholder: '-- Pilih Jenis Buah --',
